f5a59e7f3b7f4fd4c193c8384cffe9857cc566e3
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-05-13 19:03:33 +0600

    use effect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [users,setUsers]=useState([])
+  const [users,setUsers]=useState([]);
+  useEffect(()=>{
+    fetch('http://localhost:5000/users')
+    .then(res=>res.json())
+    .then(data=>console.log(data))
+
+  },[])
   return (
     <div className="App">
   
